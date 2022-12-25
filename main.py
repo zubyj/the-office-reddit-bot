5523ee1f8843e36bea15c295a64729c2fdcdf2e1
Zuby Javed <zubyjaved@gmail.com>
2022-12-25 00:53:55 -0800

    make api request to theofficescript.com using user comment and reply to comment with api response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import praw
 import os
+import requests
 
 class The_Office_Bot:
 
@@ -16,18 +17,27 @@ class The_Office_Bot:
             user_agent=os.getenv('USER_AGENT'),
             username=os.getenv('USERNAME'),
 )
-        print(self.reddit.read_only)
-
-
         self.subreddit = self.reddit.subreddit("BotsPlayHere")
 
 
     def run(self):
         for comment in self.subreddit.stream.comments():
-            print(comment)
             if not comment.saved and "michael-bot" in comment.body:
-                print('i triggered')
-                comment.reply('im triggered')
+                print('replied to ' + comment.body)
+                theComment = comment.body.replace('michael-bot', '')
+                theComment = theComment.replace(" ", "-")
+                theComment = theComment[:-1] if theComment[-1] == '-' else theComment
+                print("the comment: " + theComment)
+                response = requests.get("https://theofficescript.com/characters/michael/ask/" + theComment)
+                response = response.json()
+
+                line = response['response']
+                season = str(response['season'])
+                episode = str(response['episode'])
+
+                botResponse = line + "\n\nseason " + season + " episode: " + episode
+                print('response : ' + botResponse)
+                comment.reply(botResponse)
                 comment.save()
 
 bot = The_Office_Bot()
