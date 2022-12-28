09cd889f353ceed8a4982411aa10e8a935380114
Zuby Javed <zubyjaved@gmail.com>
2022-12-28 00:00:25 -0800

    enable jim, pam, andy, and dwight bots

--- main.py
+++ main.py
@@ -24,12 +24,26 @@ class The_Office_Bot:
         self.c = self.conn.cursor()
         self.c.execute('''CREATE TABLE IF NOT EXISTS comments (id text)''')
 
+        # Available characters that can be asked questions on The Office Script API
+        self.bots = ['michael-bot', 'dwight-bot', 'jim-bot', 'pam-bot', 'andy-bot']
+
+        self.character = 'michael'
+
+        self.names = {
+            'michael' : 'Michael Scott',
+            'dwight' : 'Dwight Schrute',
+            'pam' : 'Pam Beesly',
+            'jim' : 'Jim Halpert',
+            'andy' : 'Andy Bernard',
+        }
+
 
     # format Reddit comment to send to API
     def format_comment(self, comment):
         # remove the word michael-bot
         comment = comment.split()
-        filtered_comment = [word for word in comment if 'michael-bot' not in word]
+        botName = self.character + '-bot'
+        filtered_comment = [word for word in comment if botName not in word]
         comment = ' '.join(filtered_comment)
         # remove nonalphanumeric chars
         comment = re.sub(r'[^a-zA-Z0-9\s]', '', comment)
@@ -41,32 +55,45 @@ class The_Office_Bot:
         return comment
 
 
+    def is_valid(self, comment):
+        valid = False
+        # checks if comment contains keyword
+        for bot in self.bots:
+            if bot in comment.body.lower():
+                valid = True
+                self.character = bot.split('-')[0]
+        if not valid: return False
+
+        if comment.author == 'the-office-bot':
+            return False
+
+        # checks if comment hasnt already been replied to
+        self.c.execute('SELECT * FROM comments WHERE id=?', (comment.id,))
+        if self.c.fetchone() is not None:
+            valid = False
+        return valid
+
+
     def run(self):
         for comment in self.subreddit.stream.comments():
-            if "michael-bot" in comment.body:
-                self.c.execute('SELECT * FROM comments WHERE id=?', (comment.id,))
-
+            if self.is_valid(comment):
                 # reply if bot hasnt already replied
-                if self.c.fetchone() is None:
-                    commentBody = self.format_comment(comment.body)
+                commentBody = self.format_comment(comment.body)
 
-                    response = requests.get("https://theofficescript.com/characters/michael/ask/" + commentBody)
-                    response = response.json()
+                # get response from The Office Script API
+                response = requests.get("https://theofficescript.com/characters/" + self.character + "/ask/" + commentBody)
+                response = response.json()
 
-                    line = response['response']
-                    season = str(response['season'])
-                    episode = str(response['episode'])
-                    botResponse = line + "\n\nseason " + season + " episode: " + episode
-                    comment.reply(botResponse)
-                    print('replied to ' + comment.body + ' with response ' + botResponse)
+                line = response['response']
+                season = str(response['season'])
+                episode = str(response['episode'])
+                botResponse = line + "\n\n" + "-" + self.names[self.character]+ "\n\nSeason " + season + " Episode " + episode
+                comment.reply(botResponse)
+                print('comment ' + commentBody)
+                print('response ' + botResponse)
 
-                    self.c.execute('INSERT INTO comments VALUES (?)', (comment.id,))
-                    self.conn.commit()
-                # else:
-                #     print('already replied')
-                #     print(comment.body)
-                #     print('id ' + comment.id)
-                #     print()
+                self.c.execute('INSERT INTO comments VALUES (?)', (comment.id,))
+                self.conn.commit()
 
 
 bot = The_Office_Bot()
