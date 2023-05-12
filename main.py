46737c418442864780477407f1e70797e5e68974
Zuby Javed <zubyjaved@gmail.com>
2023-05-12 02:14:42 -0700

    update comments

--- main.py
+++ main.py
@@ -1,71 +1,67 @@
 #
-'''
+"""
 
 The Office Reddit Response Bots
-
 Created by Zuby
 
-'''
+"""
 
 
 from dotenv import load_dotenv
 import praw
-
 import os, requests, sqlite3
-
 from utils import format_comment
 
-class The_Office_Bot:
 
+class The_Office_Bot:
     def __init__(self):
         load_dotenv()
 
         # Authenticate account with Reddit API
         self.reddit = praw.Reddit(
-            client_id=os.getenv('ID'),
-            client_secret=os.getenv('SECRET'),
-            password=os.getenv('PASSWORD'),
-            user_agent=os.getenv('USER_AGENT'),
-            username=os.getenv('USERNAME'),
+            client_id=os.getenv("ID"),
+            client_secret=os.getenv("SECRET"),
+            password=os.getenv("PASSWORD"),
+            user_agent=os.getenv("USER_AGENT"),
+            username=os.getenv("USERNAME"),
         )
         self.subreddit = self.reddit.subreddit("BotsPlayHere")
 
         # Connect to the database
-        self.conn = sqlite3.connect('replied_comments.db')
+        self.conn = sqlite3.connect("replied_comments.db")
         self.c = self.conn.cursor()
-        self.c.execute('''CREATE TABLE IF NOT EXISTS comments (id text)''')
+        self.c.execute("""CREATE TABLE IF NOT EXISTS comments (id text)""")
 
         # Available characters that can be asked questions in the API
         # https://www.theofficescript.com/#ask_question_character
-        self.bots = ['michael-bot', 'dwight-bot', 'jim-bot', 'pam-bot', 'andy-bot']
-        self.character = 'michael' # The character referenced by the reddit comment
+        self.bots = ["michael-bot", "dwight-bot", "jim-bot", "pam-bot", "andy-bot"]
+        self.character = "michael"  # The character referenced by the reddit comment
         self.names = {
-            'michael' : 'Michael Scott',
-            'dwight' : 'Dwight Schrute',
-            'pam' : 'Pam Beesly',
-            'jim' : 'Jim Halpert',
-            'andy' : 'Andy Bernard',
+            "michael": "Michael Scott",
+            "dwight": "Dwight Schrute",
+            "pam": "Pam Beesly",
+            "jim": "Jim Halpert",
+            "andy": "Andy Bernard",
         }
-
 
     # Checks if our bots mentioned in reddit comment
     def is_valid(self, comment):
-        if comment.author == 'the-office-bot':
+        if comment.author == "the-office-bot":
             return False
 
         valid = False
         for bot in self.bots:
             if bot in comment.body.lower():
                 valid = True
-                self.character = bot.split('-')[0]
-        if not valid: return False
+                self.character = bot.split("-")[0]
+        if not valid:
+            return False
 
         # Ensures we havent already responded to the comment
-        self.c.execute('SELECT * FROM comments WHERE id=?', (comment.id,))
+        self.c.execute("SELECT * FROM comments WHERE id=?", (comment.id,))
         if self.c.fetchone() is not None:
             valid = False
         return valid
-
 
     def run(self):
         for comment in self.subreddit.stream.comments():
@@ -73,20 +69,34 @@ class The_Office_Bot:
                 commentBody = format_comment(self.character, comment.body)
 
                 # Make API request with given character and Reddit comment
-                response = requests.get("https://theofficescript.com/characters/" + self.character + "/ask/" + commentBody)
+                response = requests.get(
+                    "https://theofficescript.com/characters/"
+                    + self.character
+                    + "/ask/"
+                    + commentBody
+                )
                 response = response.json()
 
-                # Format JSON response to respond to user  
-                line = response['response']
-                season = str(response['season'])
-                episode = str(response['episode'])
-                botResponse = line + "\n\n" + "-" + self.names[self.character]+ "\n\nSeason " + season + " Episode " + episode
-                
+                # Format JSON response to respond to user
+                line = response["response"]
+                season = str(response["season"])
+                episode = str(response["episode"])
+                botResponse = (
+                    line
+                    + "\n\n"
+                    + "-"
+                    + self.names[self.character]
+                    + "\n\nSeason "
+                    + season
+                    + " Episode "
+                    + episode
+                )
+
                 comment.reply(botResponse)
-                print('comment : ' + commentBody + ', response : ' + botResponse)
+                print("comment : " + commentBody + ", response : " + botResponse)
 
                 # Store comment id in database (avoids replying more than once)
-                self.c.execute('INSERT INTO comments VALUES (?)', (comment.id,))
+                self.c.execute("INSERT INTO comments VALUES (?)", (comment.id,))
                 self.conn.commit()
 
 
--- utils.py
+++ utils.py
@@ -1,18 +1,17 @@
 import re
 
-
 # Format Reddit comment for API request
 def format_comment(character, comment):
     # remove the word michael-bot
     comment = comment.split()
-    botName = character + '-bot'
+    botName = character + "-bot"
     filtered_comment = [word for word in comment if botName not in word]
-    comment = ' '.join(filtered_comment)
+    comment = " ".join(filtered_comment)
     # remove nonalphanumeric chars
-    comment = re.sub(r'[^a-zA-Z0-9\s]', '', comment)
-    # remove extra whitespaces 
-    comment = ' '.join(comment.split())
-    comment = comment.replace(' ' , '-')
-    if comment[-1] == '-':
+    comment = re.sub(r"[^a-zA-Z0-9\s]", "", comment)
+    # remove extra whitespaces
+    comment = " ".join(comment.split())
+    comment = comment.replace(" ", "-")
+    if comment[-1] == "-":
         comment = comment[:-1]
     return comment
